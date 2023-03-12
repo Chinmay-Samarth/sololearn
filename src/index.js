0b02c2966eb845a34db998bfbd7def88e192e7e1
Chinmay <chinmaydhondale@gmail.com>
2023-03-12 18:59:25 +0530

    react day 2

--- src/index.js
+++ src/index.js
@@ -4,16 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-root.render(
-  <h1>Hello, React!</h1>,
+const root = ReactDOM.createRoot(
   document.getElementById('root')
-)
+);
+
+const name = "Chinmay"
+const el = <p>Hello, {name}</p>
+
+let counter = 0;
+
+function show(){
+  counter++;
+  const el = <p>{counter}</p>;
+  root.render(
+    el, document.getElementById('root')
+  );
+}
+
+//? setInterval(show,1000);
+
+function Hello(){
+  return <h1>Hello world.</h1>;
+}
+
+const line = <Hello />;
+
+root.render(
+  line,
+  document.getElementById('root')
+);
+
+class classComponent extends React.Component {
+  render(){
+    return <h1>Hello world.</h1>
+  }
+}
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
